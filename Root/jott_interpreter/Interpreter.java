0337f3e660b7963d3415d80d7a080a30c4817971
Jiancheng Wu <iw1317@rit.edu>
2024-09-07 15:55:35 -0400

    implemented scanning v1

--- Root/jott_interpreter/Interpreter.java
+++ Root/jott_interpreter/Interpreter.java
@@ -1,6 +1,7 @@
 package jott_interpreter;
 
 import java.util.ArrayList;
+import java.util.Scanner;
 
 import provided.*;
 
@@ -11,6 +12,16 @@ public class Interpreter {
     }
 
     public static ArrayList<Token> tokenize(String filename) {
+        // File scanning
+        Scanner scanner = new Scanner(filename);
+        while (scanner.hasNext()) {
+            char c = scanner.next();
+            // Cases
+            if () {
+                
+            }
+        }
+
         return null; // TODO: Implement this method
     }
 
