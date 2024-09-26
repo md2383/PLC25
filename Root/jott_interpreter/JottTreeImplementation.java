12bdbd425e18e9f8ab70b3d5287d4709ac032032
Jacob Locke <jacobalocke1@gmail.com>
2024-09-26 19:09:15 -0400

    stubbed tree implementation

---
+++ Root/jott_interpreter/JottTreeImplementation.java
@@ -0,0 +1,25 @@
+package jott_interpreter;
+
+import provided.JottTree;
+
+public class JottTreeImplementation implements JottTree{
+
+    @Override
+    public String convertToJott() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'convertToJott'");
+    }
+
+    @Override
+    public boolean validateTree() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'validateTree'");
+    }
+
+    @Override
+    public void execute() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'execute'");
+    }
+    
+}
