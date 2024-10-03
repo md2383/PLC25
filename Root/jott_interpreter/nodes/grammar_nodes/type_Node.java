90d3a7688347ace0799dd2833ea278fc1bf9f51c
Jiancheng Wu <iw1317@rit.edu>
2024-10-03 18:57:02 -0400

    Type created

---
+++ Root/jott_interpreter/nodes/grammar_nodes/type_Node.java
@@ -0,0 +1,20 @@
+package jott_interpreter.nodes.grammar_nodes;
+
+import java.util.ArrayList;
+import jott_interpreter.SyntaxError;
+import jott_interpreter.nodes.Jott_Node;
+import provided.Token;
+import provided.TokenType;
+
+public class type_Node extends Jott_Node {
+    public static params_Node parseTypeNode(ArrayList<Token> tokens) throws SyntaxError {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'parseTypeNode'");
+    }
+
+    @Override
+    public String convertToJott() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'convertToJott'");
+    }
+}
