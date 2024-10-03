6f97f48249c5af1d86a2acefce47de58db4de613
Jiancheng Wu <iw1317@rit.edu>
2024-10-03 19:03:47 -0400

    setted up Type Node for implementation

--- Root/jott_interpreter/nodes/token_nodes/type_Node.java
+++ Root/jott_interpreter/nodes/token_nodes/type_Node.java
@@ -4,10 +4,9 @@ import java.util.ArrayList;
 import jott_interpreter.SyntaxError;
 import jott_interpreter.nodes.Jott_Node;
 import provided.Token;
-import provided.TokenType;
 
 public class type_Node extends Jott_Node {
-    public static params_Node parseTypeNode(ArrayList<Token> tokens) throws SyntaxError {
+    public static type_Node parseTypeNode(ArrayList<Token> tokens) throws SyntaxError {
         // TODO Auto-generated method stub
         throw new UnsupportedOperationException("Unimplemented method 'parseTypeNode'");
     }
