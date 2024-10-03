fbb2c3da89cf474e1bcfa854bb7f05f31851db33
md2383 <md2383@rit.edu>
2024-10-03 18:54:20 -0400

    Fixed Bool Node

--- Root/jott_interpreter/nodes/grammar_nodes/bool_Node.java
+++ Root/jott_interpreter/nodes/grammar_nodes/bool_Node.java
@@ -1,51 +1,69 @@
 package jott_interpreter.nodes.grammar_nodes;
 
 import java.util.ArrayList;
-
+import jott_interpreter.SyntaxError;
 import jott_interpreter.nodes.Jott_Node;
 import provided.Token;
 import provided.TokenType;
 
+/**
+ * A bool representation for the parse tree
+ */
 public class bool_Node extends Jott_Node {
 
-    private ArrayList<Token> Tokens;
-    private ArrayList<Jott_Node> children;
+    /** Valid {@code ID_KEYWORD} token reference */
+    private final Token bool;
 
-    public bool_Node() {
-        super();
+    /**
+     * Private Constructor 
+     * (validation of the node done in {@link #parseBoolNode})
+     * @param boolToken a validated {@code ID_KEYWORD} token reference
+     */
+    private bool_Node(Token boolToken) {
+        this.bool = boolToken;
     }
 
-    public bool_Node(ArrayList<Token> tokens) {
-        super();
-        this.Tokens = tokens;   
+    /**
+     * Static parse method returning a {@link bool_Node} for the parse tree.
+     * 
+     * @param tokens -  the list of tokens being parsed into a parse tree
+     * @return  A bool node which has been validated in accordance 
+     *          with the parse tree grammar
+     * @throws SyntaxError  {@code Unexpected EOF}: no token to parse
+     * @throws SyntaxError  {@code Token type not ID_KEYWORD}: token being 
+     *                      parsed is not a {@code TokenType.ID_KEYWORD}
+     * @throws SyntaxError  {@code Invalid Token}: token being parsed is not 
+     *                      the expected token
+     * @implNote    The token(s) in the input array list of {@code Token} 
+     *              objects will be removed from the list given validation 
+     *              success.
+     * @see {@link Token} 
+     * @see {@link TokenType}
+     */
+    public static bool_Node parseBoolNode(ArrayList<Token> tokens) throws SyntaxError {
+        if (tokens.size() < 1) {
+            throw new SyntaxError("Unexpected EOF");
+        }
+        if (tokens.get(0).getTokenType() != TokenType.ID_KEYWORD) {
+            throw new SyntaxError("Token type not ID/Keyword");
+        }
+        if (!tokens.get(0).getToken().equals("True") && !tokens.get(0).getToken().equals("False")) {
+            throw new SyntaxError("Invalid Token: Expected 'True' or 'False'");
+        }
+        if (!Character.isUpperCase(tokens.get(0).getToken().charAt(0))) {
+            throw new SyntaxError("Token is an ID, expected 'True' or 'False'");
+        }
+        
+        return new bool_Node(tokens.remove(0));
     }
 
-    public ArrayList<Jott_Node> getChildren() {
-        return this.children;
-    }
-
-    public void setChildren(ArrayList<Jott_Node> children) {
-        this.children = children;
-    }
-
-    public ArrayList<Token> getTokens() {
-        return this.Tokens;
-    }
-
-    public void setTokens(ArrayList<Token> Tokens) {
-        this.Tokens = Tokens;
-    }
-
+    /**
+     * Converts the bool node to Jott code
+     * 
+     * @return  the Jott code representation of the bool node
+     */
     @Override
-    public boolean validate() {
-        if (this.getChildren().size() != 1) {
-            return false;
-        }
-        if (this.Tokens.get(0).getTokenType() == TokenType.ID_KEYWORD
-                && (this.Tokens.get(0).getToken().equals("True") || this.Tokens.get(0).getToken().equals("False"))) {
-            return true;
-        }
-        return false;
+    public String convertToJott() {
+        return this.bool.getToken();
     }
-    
 }
