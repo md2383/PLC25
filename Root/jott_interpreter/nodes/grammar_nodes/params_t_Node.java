79ffea97657d6982bea9922e785d613832eb00d6
Jiancheng Wu <iw1317@rit.edu>
2024-10-06 19:54:09 -0400

    param_t created

---
+++ Root/jott_interpreter/nodes/grammar_nodes/params_t_Node.java
@@ -0,0 +1,21 @@
+package jott_interpreter.nodes.grammar_nodes;
+
+import java.util.ArrayList;
+import jott_interpreter.SyntaxError;
+import jott_interpreter.nodes.*;
+import provided.*;
+
+public class params_t_Node extends Jott_Node {
+
+    public static params_t_Node parseParamsTNode(ArrayList<Token> tokens) throws SyntaxError {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'parseParamsNode'");
+    }
+
+    @Override
+    public String convertToJott() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'convertToJott'");
+    }
+    
+}
