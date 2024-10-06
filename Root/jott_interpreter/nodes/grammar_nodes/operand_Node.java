4f38c6c0f353159ab4c5c14516612f442cfeb0fb
Jiancheng Wu <iw1317@rit.edu>
2024-10-05 23:01:50 -0400

    operand node created?

---
+++ Root/jott_interpreter/nodes/grammar_nodes/operand_Node.java
@@ -0,0 +1,11 @@
+package jott_interpreter.nodes.grammar_nodes;
+
+import java.util.ArrayList;
+
+import jott_interpreter.SyntaxError;
+import jott_interpreter.nodes.*;
+import provided.*;
+
+public class operand_Node extends Jott_Node {
+    
+}
