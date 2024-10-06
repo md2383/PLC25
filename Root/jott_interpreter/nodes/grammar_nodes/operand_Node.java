be1f05d1bcb90dccc8f35e1934fc683a9259a671
Jiancheng Wu <iw1317@rit.edu>
2024-10-05 23:45:10 -0400

    operand node implemented

--- Root/jott_interpreter/nodes/grammar_nodes/operand_Node.java
+++ Root/jott_interpreter/nodes/grammar_nodes/operand_Node.java
@@ -1,19 +1,58 @@
 package jott_interpreter.nodes.grammar_nodes;
 
 import java.util.ArrayList;
+import jott_interpreter.SyntaxError;
 import jott_interpreter.nodes.*;
+import jott_interpreter.nodes.token_nodes.id_Node;
+import jott_interpreter.nodes.token_nodes.number_Node;
 import provided.Token;
+import provided.TokenType;
 
 public class operand_Node extends Jott_Node{
+    private Jott_Node child;
+    private boolean neg;
 
-    public static Jott_Node parseOperandNode(ArrayList<Token> tokens) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'parseOperandNode'");
+    public operand_Node(Jott_Node node) {
+        this.child = node;
+        this.neg = false;
+    }
+
+    public operand_Node(Jott_Node node, boolean neg) {
+        this.child = node;
+        this.neg = neg;
+    }
+
+    public static operand_Node parseOperandNode(ArrayList<Token> tokens) throws SyntaxError {
+        if (tokens.size() < 1) {
+            throw new SyntaxError("Unexpected EOF");
+        }
+        Jott_Node temp;
+        if (tokens.get(0).getTokenType() == TokenType.ID_KEYWORD) {
+            temp = id_Node.parseIdNode(tokens);
+        } else if (tokens.get(0).getTokenType() == TokenType.NUMBER) {
+            temp = number_Node.parseNumberNode(tokens);
+        } else if (tokens.get(0).getTokenType() == TokenType.ID_KEYWORD) {
+            temp = functionCall_Node.parseFunctionCallNode(tokens);
+        } else if (tokens.get(0).getTokenType() == TokenType.MATH_OP) {
+            if (tokens.get(0).getToken().equals("-")) {
+                tokens.remove(0);
+                temp = number_Node.parseNumberNode(tokens);
+                return new operand_Node(temp, true);
+            } else {
+                throw new SyntaxError("Invalid MATH_OP operator \"" + tokens.get(0).getToken() + "\", expected \"-\"");
+            }
+        } else {
+            throw new SyntaxError("Invalid token type");
+        }
+        return new operand_Node(temp);
     }
 
     @Override
     public String convertToJott() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'convertToJott'");
+        if (this.neg == true) {
+            return "-" + this.child.convertToJott();
+        } else {
+            return this.child.convertToJott();
+        }
     }
 }
