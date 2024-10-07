d11c4053e1ccb087811d69a7bed42881d2b670bc
Jiancheng Wu <iw1317@rit.edu>
2024-10-06 20:07:38 -0400

    implemented param_t node

--- Root/jott_interpreter/nodes/grammar_nodes/params_t_Node.java
+++ Root/jott_interpreter/nodes/grammar_nodes/params_t_Node.java
@@ -6,16 +6,26 @@ import jott_interpreter.nodes.*;
 import provided.*;
 
 public class params_t_Node extends Jott_Node {
+    private expr_Node expr;
+
+    public params_t_Node(expr_Node expr) {
+        this.expr = expr;
+    }
 
     public static params_t_Node parseParamsTNode(ArrayList<Token> tokens) throws SyntaxError {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'parseParamsNode'");
+        if (tokens.size() < 1) {
+            throw new SyntaxError("Unexpected EOF");
+        }
+        if (tokens.get(0).getTokenType() != TokenType.COMMA) {
+            throw new SyntaxError("Invalid token type, expected \",\"");
+        }
+        tokens.remove(0);
+        return new params_t_Node(expr_Node.parseExprNode(tokens));
     }
 
     @Override
     public String convertToJott() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'convertToJott'");
+        return ", " + this.expr.convertToJott();
     }
     
 }
