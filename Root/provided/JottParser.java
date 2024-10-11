e1794fba8ba3ff876e20ebd915b3081da7f37468
jal7957 <jal7957@rit.edu>
2024-10-11 12:20:09 -0400

    Temporary JottParser implementation for testing

--- Root/provided/JottParser.java
+++ Root/provided/JottParser.java
@@ -1,23 +1,37 @@
 package provided;
 
-/**
- * This class is responsible for paring Jott Tokens
- * into a Jott parse tree.
- *
- * @author
- */
-
 import java.util.ArrayList;
 
+import jott_interpreter.SyntaxError;
+import jott_interpreter.nodes.grammar_nodes.program_Node;
+
+/**
+ * This class is responsible for parsing Jott Tokens
+ * into a Jott parse tree.
+ */
 public class JottParser {
 
     /**
-     * Parses an ArrayList of Jotton tokens into a Jott Parse Tree.
+     * Parses an ArrayList of Jott tokens into a Jott Parse Tree.
      * @param tokens the ArrayList of Jott tokens to parse
      * @return the root of the Jott Parse Tree represented by the tokens.
      *         or null upon an error in parsing.
      */
     public static JottTree parse(ArrayList<Token> tokens){
-		return null;
+      JottTree root;
+
+      try {
+        root = program_Node.parseProgramNode(tokens);
+      } catch (SyntaxError Error) {
+        if(tokens.size() != 0) {
+          Token errorToken = tokens.get(0);
+          Error.print(errorToken.getFilename(), errorToken.getLineNum());
+        } else {
+          Error.print("", 0);
+        }
+        return null;
+      }
+
+      return root;
     }
 }
