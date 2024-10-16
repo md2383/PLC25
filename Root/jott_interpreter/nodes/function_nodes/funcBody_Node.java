e0dd50d450fca1a5d472c30c5f1223d72b1e6ca8
Jacob Locke <jacobalocke1@gmail.com>
2024-10-16 09:39:01 -0400

    Implemented funcBody_Node

--- Root/jott_interpreter/nodes/function_nodes/funcBody_Node.java
+++ Root/jott_interpreter/nodes/function_nodes/funcBody_Node.java
@@ -4,18 +4,43 @@ import java.util.ArrayList;
 
 import jott_interpreter.SyntaxError;
 import jott_interpreter.nodes.*;
+import jott_interpreter.nodes.grammar_nodes.body_Node;
+import jott_interpreter.nodes.grammar_nodes.varDec_Node;
 import provided.*;
 
 public class funcBody_Node extends Jott_Node{
+
+    private final varDec_Node[] vars;
+    private final body_Node body;
+
+    private funcBody_Node(varDec_Node[] variableDeclarations, body_Node body) {
+        this.vars = variableDeclarations;
+        this.body = body;
+    }
     
     public static funcBody_Node parseFuncBodyNode(final ArrayList<Token> tokens) throws SyntaxError {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'parseParamsNode'");
+        if (tokens.size() < 1){ throw new SyntaxError("Unexpected EOF"); }
+
+        ArrayList<varDec_Node> tempVars = new ArrayList<>();
+
+        while(  tokens.get(0).getToken().equals("Double")   ||
+                tokens.get(0).getToken().equals("Integer")  ||
+                tokens.get(0).getToken().equals("String")   ||
+                tokens.get(0).getToken().equals("Boolean")  ) {
+            tempVars.add(varDec_Node.parseVarDecNode(tokens));
+            if (tokens.size() < 1){ throw new SyntaxError("Unexpected EOF"); }
+        }
+
+        body_Node body = body_Node.parseBodyNode(tokens);
+
+        return new funcBody_Node((varDec_Node[])(tempVars.toArray()), body);
     }
 
     @Override
     public String convertToJott() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'convertToJott'");
+        StringBuilder str = new StringBuilder();
+        for(varDec_Node var : this.vars) { str.append(var.convertToJott()); }
+        str.append(body.convertToJott());
+        return str.toString();
     }
 }
