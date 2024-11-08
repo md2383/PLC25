b2f8821fb9b67b06846e3a114444348c7e6b8bb3
Jacob Locke <jacobalocke1@gmail.com>
2024-11-08 10:50:06 -0500

    Reorganized the main function and implemented the correct file structure

---
+++ Root/Jott.java
@@ -0,0 +1,67 @@
+import java.util.ArrayList;
+
+import jott_interpreter.Interpreter;
+import provided.JottParser;
+import provided.JottTree;
+import provided.Token;
+
+public class Jott {
+    
+    public static void main(String[] args) {
+        String filename;
+        ArrayList<Token> tokens;
+        JottTree root = null;
+        boolean valid_semantics = false;
+
+        /** Get the filename from the command line arguments */
+        filename = args[0];
+
+        //  PHASE 1
+        /** Tokenize the file */
+        tokens = Interpreter.tokenize(filename);
+
+        //  PHASE 2
+        /** Parse the tokens into a root node of the JottTree */
+        try{
+
+            root = JottParser.parse(tokens);
+
+        } catch (Exception e) {
+            System.err.println("Error Parsing Tokens: \n");
+            e.printStackTrace();
+            System.exit(1);
+        }
+
+        //! Prematurely End Program: 
+        //  Incorrect Parse Grammar
+        if(root == null) { return; }
+
+        //  PHASE 3
+        /** Validate the ParseTree */
+        try {
+
+            valid_semantics = root.validateTree();
+
+        } catch (Exception e) {
+            System.err.println("Error Validating JottTree: \n");
+            e.printStackTrace();
+            System.exit(1);
+        }
+        
+        //! Prematurely End Program: 
+        //  Invalid Jott Semantics
+        if(!valid_semantics) { return; }
+
+        //  PHASE 4
+        /** Evaluate the JottTree */
+        try {
+
+            // root.execute(); // commented out until phase 4
+
+        } catch (Exception e) {
+            System.err.println("Error Evaluating JottTree: \n");
+            e.printStackTrace();
+            System.exit(1);
+        }
+    }
+}
